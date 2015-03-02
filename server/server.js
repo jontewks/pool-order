eef9cdf9ae77716544e74bb8dc18f7f4ca13e146
Jon Tewksbury <jontewks@gmail.com>
2015-03-01 22:14:58 -0800

    Fix port.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var base = require('./routes/base');
 
+app.set('port', (process.env.PORT || 5000))
+
 app.use('/', base);
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
   var port = server.address().port;
   console.log('Example app listening at http://localhost:%s', port);
 });
