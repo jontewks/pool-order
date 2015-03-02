59945a09d66948a45b09d27b931622f908645ed9
Jon Tewksbury <jontewks@gmail.com>
2015-03-01 22:34:10 -0800

    Add names for shuffling.

--- server/routes/base.js
+++ server/routes/base.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 router.get('/', function(req, res) {
-  res.send('Sup');
+  var names = [
+    'Dustin',
+    'Jesse',
+    'Brad',
+    'Jon',
+    'Joe',
+    'Alex',
+    'Nate'
+  ];
+
+  res.send(_.shuffle(names));
 });
 
 module.exports = router;
