8e432ab4ec6b91eaf205c27609e87a561f195e14
Jon Tewksbury <jontewks@gmail.com>
2015-03-04 22:59:23 -0800

    More react.

--- build/index.js
+++ build/index.js
@@ -1,4 +1,46 @@
-React.render(
-  React.createElement("h1", null, "Hello, world!"),
-  document.getElementById('mount-point')
-);
+var List = React.createClass({displayName: "List",
+  getInitialState: function() {
+    return {
+      names: []
+    };
+  },
+
+  shuffle: function() {
+    console.log(this);
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        React.createElement(Buttons, {names: this.props.names}), 
+        React.createElement("button", {onClick: this.shuffle}, "Shuffle")
+      )
+    );
+  }
+});
+
+var Buttons = React.createClass({displayName: "Buttons",
+  render: function() {
+    var buttons = this.props.names.map(function(name) {
+      return (
+        React.createElement("input", {type: "checkbox", key: name}, 
+          name
+        )
+      );
+    });
+
+    return React.createElement("div", null, buttons)
+  }
+});
+
+var names = [
+  'Dustin',
+  'Jesse',
+  'Brad',
+  'Jon',
+  'Joe',
+  'Alex',
+  'Nate'
+];
+
+React.render(React.createElement(List, {names: names}), document.getElementById('mount-point'));
--- client/index.js
+++ client/index.js
@@ -1,4 +1,46 @@
-React.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('mount-point')
-);
+var List = React.createClass({
+  getInitialState: function() {
+    return {
+      names: []
+    };
+  },
+
+  shuffle: function() {
+    console.log(this);
+  },
+
+  render: function() {
+    return (
+      <div>
+        <Buttons names={this.props.names} />
+        <button onClick={this.shuffle}>Shuffle</button>
+      </div>
+    );
+  }
+});
+
+var Buttons = React.createClass({
+  render: function() {
+    var buttons = this.props.names.map(function(name) {
+      return (
+        <input type='checkbox' key={name}>
+          {name}
+        </input>
+      );
+    });
+
+    return <div>{buttons}</div>
+  }
+});
+
+var names = [
+  'Dustin',
+  'Jesse',
+  'Brad',
+  'Jon',
+  'Joe',
+  'Alex',
+  'Nate'
+];
+
+React.render(<List names={names} />, document.getElementById('mount-point'));
